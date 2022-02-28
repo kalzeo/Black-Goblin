5d5365445bf9a625b83f52eef8aca041151cf637
Kyle <ixkalz@gmail.com>
2022-02-28 05:07:33 +0000

    Updated run.bat, included class index worksheet

--- run.bat
+++ run.bat
@@ -1,1 +1,11 @@
-python track.py --project "./tests/SCENE_BadBlood" --name "SCENE_BadBlood_yolov5m" --source "./videos/SCENE_BadBlood.mp4" --deep_sort_model "densenet201" --yolo_model "yolov5s.pt" --classes 0
+:: IMPORTANT: make sure there's no spaces between the variable and it's value i.e. set test="test", set test=0, etc.
+:: Class indexes for the classes variable must seperated by spaces after the first number
+
+set proj="./tests/SCENE_BadBlood"     & :: The directory for the project, paste the path between the quotes
+set name="SCENE_BadBlood_yolov5m"     & :: The project name
+set src="./videos/SCENE_BadBlood.mp4" & :: Path to the video
+set ym="yolov5s.pt"                   & :: The YOLO model
+set ds="densenet201"                  & :: The Deep SORT model
+set classes=0 1 2 3                   & :: The class indexes for the model to identify (see 'Class Indexes.xlsx') seperated by spaces if classes > 1
+
+python track.py --project %proj% --name %name% --source %src% --deep_sort_model %ds% --yolo_model %ym% --classes %classes%
