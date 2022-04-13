2c14421808f2847e8410df5a7fdc1f3d001e866f
Kyle <ixkalz@gmail.com>
2022-04-13 11:37:43 +0100

    Added some results

--- run.bat
+++ run.bat
@@ -1,10 +1,19 @@
 :: IMPORTANT: make sure there's no spaces between the variable and it's value i.e. set test="test", set test=0, etc.
 :: Class indexes for the classes variable must seperated by spaces after the first number i.e. 0 1 2
 
+:: The default model used is YOLOv5l. However, this default can be changed in track.py on line 261 to YOLOv5[s,m,l,x]
+
 @echo off
-set proj="./tests/SCENE_BadBlood"     & :: The directory for the project, paste the path between the quotes
-set name="SCENE_BadBlood_yolov5l"     & :: The project name
-set src="./videos/SCENE_BadBlood.mp4" & :: Path to the video
-set classes=0                         & :: The class indexes for the model to identify (see 'Class Indexes.xlsx') seperated by spaces if number of classes > 1
+:: The directory for the project, paste the path between the quotes
+set proj="./tests/SCENE_BadBlood"
+
+:: The project name
+set name="SCENE_BadBlood_yolov5l"
+
+:: Path to the video
+set src="./videos/SCENE_BadBlood.mp4"
+
+:: The class indexes for the model to identify (see 'Class Indexes.xlsx') seperated by spaces if number of classes > 1
+set classes = 0
 
 python track.py --project %proj% --name %name% --source %src% --classes %classes%
